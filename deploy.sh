ccdb7c992e9a3874b78c85c483413c7ff629238c
syl <18353232271@163.com>
2021-07-20 11:16:09 +0800

    deploy and jenkinsfile change

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,23 @@
 #! /bin/sh
-harbor_url = $1
-harbor_project_name = $2
-project_name = $3
-project_port = $4
-tag = $5
+harbor_url=$1
+harbor_project_name=$2
+project_name=$3
+project_port=$4
+tag=$5
 
-image_name = $harbor_url/$harbor_project_name/$project_name
-tag_name = $image_name:$tag
+image_name=$harbor_url/$harbor_project_name/$project_name
+tag_name=$image_name:$tag
 
-containerId = `docker ps -a | grep -w ${image_name} | awk '{print $1}'`
+containerId=`docker ps -a | grep -w ${image_name} | awk '{print $1}'`
 if [ "$containerId" != "" ]; then
-    docker stop $containerId
-    docker rm $containerId
+  docker stop $containerId
+  docker rm $containerId
 fi
-imageId = `docker images | grep -w ${image_name} | awk '{print $3}'`
+imageId=`docker images | grep -w ${image_name} | awk '{print $3}'`
 if [ "$imageId" != "" ]; then
-    docker rmi $imageId
+  docker rmi $imageId
 fi
+
 docker login -u sam -p Sam123456 $harbor_url
 docker pull $tag_name
 docker run -d --restart=always -p $project_port:$project_port $tag_name
